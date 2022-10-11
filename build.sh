b05dd1045de6609d8597501cd43814d1b3124d59
Andrew Gunsch <andrew@gunsch.cc>
2022-10-10 21:05:00 -0700

    Adds more HTML to build

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ node_modules/less/bin/lessc site.less > "${OUTPUT_DIR}/site.css"
 
 # Copy assets
 cp -r font/ icon/ "${OUTPUT_DIR}"
-cp index.html "${OUTPUT_DIR}"
+cp *.html "${OUTPUT_DIR}"
 
 # Do the docker build!
 docker build --pull --rm -f "Dockerfile" -t gunsch.cc:latest "."
